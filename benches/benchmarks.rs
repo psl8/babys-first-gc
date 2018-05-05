212e001a8a670a643e9c30009a9b74c35644e3df
Patrick Loughney <patrickloughney0@gmail.com>
2018-05-05 12:33:21 -0400

    Add more benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -39,3 +39,37 @@ fn bench_unsafe_gc(b: &mut Bencher) {
     });
 }
 
+#[bench]
+fn long_bench_safe_gc(b: &mut Bencher) {
+    let mut vm = safe_gc::Vm::new();
+    b.iter(|| {
+        let num_objects = black_box(1 << 16);
+        for i in 0..num_objects {
+            vm.push_int(i);
+        }
+        for _ in 0..(num_objects - 1) {
+            vm.push_pair();
+        }
+
+        vm.drop();
+        vm.gc();
+    });
+}
+
+#[bench]
+fn long_bench_unsafe_gc(b: &mut Bencher) {
+    let mut vm = unsafe_gc::Vm::new();
+    b.iter(|| {
+        let num_objects = black_box(1 << 16);
+        for i in 0..num_objects {
+            vm.push_int(i);
+        }
+        for _ in 0..(num_objects - 1) {
+            vm.push_pair();
+        }
+
+        vm.drop();
+        vm.gc();
+    });
+}
+
