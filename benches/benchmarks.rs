c1bcef3fe55bb4ef59201d7fcb0b040c2e452948
Patrick Loughney <patrickloughney0@gmail.com>
2018-05-09 16:10:03 -0400

    Add idiomatic wrapper around C GC
    This gives the C GC an API identical to the Rust GCs’, as well as
    generally cleaning up the c_gc directory.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -2,7 +2,7 @@
 extern crate babys_first_gc as gc;
 extern crate test;
 
-use gc::{safe_gc, unsafe_gc};
+use gc::{safe_gc, unsafe_gc, c_gc};
 use test::*;
 
 #[bench]
@@ -73,47 +73,38 @@ fn long_bench_unsafe_gc(b: &mut Bencher) {
     });
 }
 
-use gc::c_gc::c_gc;
 #[bench]
 fn bench_c_gc(b: &mut Bencher) {
-    unsafe {
-        let vm = c_gc::newVM();
+    let mut vm = c_gc::Vm::new();
 
-        b.iter(|| {
-            let num_objects = black_box(64);
-            for i in 0..num_objects {
-                c_gc::pushInt(vm, i);
-            }
-            for _ in 0..(num_objects - 1) {
-                c_gc::pushPair(vm);
-            }
+    b.iter(|| {
+        let num_objects = black_box(64);
+        for i in 0..num_objects {
+            vm.push_int(i);
+        }
+        for _ in 0..(num_objects - 1) {
+            vm.push_pair();
+        }
 
-            c_gc::pop(vm);
-            c_gc::gc(vm);
-        });
-
-        c_gc::freeVM(vm);
-    }
+        vm.drop();
+        vm.gc();
+    });
 }
 
 #[bench]
 fn long_bench_c_gc(b: &mut Bencher) {
-    unsafe {
-        let vm = c_gc::newVM();
+    let mut vm = c_gc::Vm::new();
 
-        b.iter(|| {
-            let num_objects = black_box(1 << 16);
-            for i in 0..num_objects {
-                c_gc::pushInt(vm, i);
-            }
-            for _ in 0..(num_objects - 1) {
-                c_gc::pushPair(vm);
-            }
+    b.iter(|| {
+        let num_objects = black_box(1 << 16);
+        for i in 0..num_objects {
+            vm.push_int(i);
+        }
+        for _ in 0..(num_objects - 1) {
+            vm.push_pair();
+        }
 
-            c_gc::pop(vm);
-            c_gc::gc(vm);
-        });
-
-        c_gc::freeVM(vm);
-    }
+        vm.drop();
+        vm.gc();
+    });
 }
--- src/c_gc/mod.rs
+++ src/c_gc/mod.rs
@@ -1,4 +1,92 @@
 #[allow(non_snake_case, non_upper_case_globals)]
-pub mod c_gc;
+mod c_gc_bindings;
+
+pub use self::c_gc_bindings::{STACK_MAX, ObjectType_OBJ_INT, ObjectType_OBJ_PAIR};
+use self::c_gc_bindings::*;
+
+pub struct Vm(*mut VM);
+
+impl Vm {
+    pub fn new() -> Self {
+        unsafe {
+            Vm(newVM())
+        }
+    }
+
+    pub fn push_int(&mut self, i: i32) {
+        unsafe {
+            pushInt(self.0, i as ::std::os::raw::c_int);
+        }
+    }
+
+    pub fn push_pair(&mut self) {
+        unsafe {
+            pushPair(self.0);
+        }
+    }
+
+    pub fn gc(&mut self) {
+        unsafe {
+            gc(self.0);
+        }
+    }
+
+    pub fn drop(&mut self) {
+        unsafe {
+            pop(self.0);
+        }
+    }
+}
+
+impl Drop for Vm {
+    fn drop(&mut self) {
+        unsafe {
+            freeVM(self.0);
+        }
+    }
+}
+
+use std::ops::{Deref, DerefMut};
+impl Deref for Vm {
+    type Target = VM;
+
+    fn deref(&self) -> &VM {
+        unsafe {
+            &*self.0
+        }
+    }
+}
+impl DerefMut for Vm {
+    fn deref_mut(&mut self) -> &mut VM {
+        unsafe {
+            &mut *self.0
+        }
+    }
+}
+
 #[cfg(test)]
-mod c_gc_test;
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_c_gc() {
+        let mut vm = Vm::new();
+
+        for i in 0..64 {
+            vm.push_int(i);
+        }
+        for _ in 0..63 {
+            vm.push_pair();
+        }
+
+        assert_ne!(vm.numObjects, 0);
+        assert!(!vm.firstObject.is_null());
+
+        vm.drop();
+
+        vm.gc();
+
+        assert_eq!(vm.numObjects, 0);
+        assert!(vm.firstObject.is_null());
+    }
+}
